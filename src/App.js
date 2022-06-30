0da798f1119e36deb877ebbb4d5a4a4aad95c4ec
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-06-30 20:52:41 +0200

    adding hooks to search.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import React from "react";
-import Accordion from "./components/Accordion";
+// import Accordion from "./components/Accordion";
 import Search from "./components/Search";
 
 const App = () => {
-  const items = [
-    {
-      title: "What is Reaact?",
-      content: "React is a front end javascript framework.",
-    },
-    {
-      title: "Why use React?",
-      content: "React is a favorite JS library among engineers.",
-    },
-    {
-      title: "How do you use React?",
-      content: "You use React by creating React components.",
-    },
-  ];
+  // const items = [
+  //   {
+  //     title: "What is Reaact?",
+  //     content: "React is a front end javascript framework.",
+  //   },
+  //   {
+  //     title: "Why use React?",
+  //     content: "React is a favorite JS library among engineers.",
+  //   },
+  //   {
+  //     title: "How do you use React?",
+  //     content: "You use React by creating React components.",
+  //   },
+  // ];
 
   return (
     <div>
