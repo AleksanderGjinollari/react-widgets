29bd3195444c4d4799968c070ae044f59fc0e47f
Aleksander Gjinollari <aleksandergjinollari@hotmail.com>
2022-07-04 12:02:38 +0200

    added the color list

--- src/App.js
+++ src/App.js
@@ -3,41 +3,41 @@ import React from "react";
 // import Search from "./components/Search";
 import Dropdown from "./components/Dropdown";
 
-const App = () => {
-  // const items = [
-  //   {
-  //     title: "What is Reaact?",
-  //     content: "React is a front end javascript framework.",
-  //   },
-  //   {
-  //     title: "Why use React?",
-  //     content: "React is a favorite JS library among engineers.",
-  //   },
-  //   {
-  //     title: "How do you use React?",
-  //     content: "You use React by creating React components.",
-  //   },
-  // ];
+// const items = [
+//   {
+//     title: "What is Reaact?",
+//     content: "React is a front end javascript framework.",
+//   },
+//   {
+//     title: "Why use React?",
+//     content: "React is a favorite JS library among engineers.",
+//   },
+//   {
+//     title: "How do you use React?",
+//     content: "You use React by creating React components.",
+//   },
+// ];
+const options = [
+  {
+    label: "The Color Red",
+    value: "red",
+  },
+  {
+    label: "The Color Green",
+    value: "green",
+  },
+  {
+    label: "The Color Blue",
+    value: "blue",
+  },
+];
 
-  const options = [
-    {
-      label: "The Color Red",
-      value: "red",
-    },
-    {
-      label: "The Color Green",
-      value: "green",
-    },
-    {
-      label: "The Color Blue",
-      value: "blue",
-    },
-  ];
-
+const exportedColors = () => {
   return (
     <div>
-      <Dropdown optons={options} />
+      <Dropdown options={options}></Dropdown>
     </div>
   );
 };
-export default App;
+
+export default exportedColors;
